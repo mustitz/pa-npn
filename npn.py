3f7069840795f0c84f7cd9ddfa5da84f90d6802a
Mustitz <mustitz@gmail.com>
2021-08-24 16:35:46 +0000

    Add an output format support

--- npn.py
+++ npn.py
@@ -4,6 +4,29 @@ import sys
 import numpy as np
 
 
+def _dump_dec(value, width=None):
+    value = str(value)
+    return value.rjust(width) if width else value
+
+def _dump_hex(value, width=None):
+    value = hex(value)[2:]
+    return value.zfill(width) if width else value
+
+def _dump_bit(value, width=None):
+    value = f"{value:b}"
+    return value.zfill(width) if width else value
+
+FORMATS = {
+    '#': (None, 'num', _dump_dec),
+    'H': ('hex_func', 'func', _dump_hex),
+    'B': ('bit_func', 'func', _dump_bit),
+    'D': ('dec_func', 'func', _dump_dec),
+    'N': ('arg_count', 'qargs', _dump_dec),
+    'Q': ('one_count', 'qones', _dump_dec),
+    'S': (None, 'npn_class_len', _dump_dec),
+}
+
+
 def ncm(x): # pylint: disable=invalid-name
     """
     Generate the next combination mask (NCM) for an integer.
@@ -75,6 +98,13 @@ def get_npn_class(qvalues, func, transforms, bits):
     return npn_class_len
 
 
+def _out(out_format, **kwargs):
+    for fmt, width in out_format:
+        _, name, dump = FORMATS[fmt]
+        value = kwargs[name]
+        yield dump(value, width)
+
+
 def build_npn_classes(qargs, qones_range=None, **kwargs):
     """
     Build all NPN classes in an optimal way for a given argument count.
@@ -83,6 +113,14 @@ def build_npn_classes(qargs, qones_range=None, **kwargs):
     qvalues = 2 ** qargs
     qfunctions = 2 ** qvalues
 
+    out_format = kwargs.pop('out_format', None)
+    if out_format:
+        out_format = out_format[:]
+    else:
+        out_format = parse_out_format('#BS')
+
+    set_default_width(out_format, qvalues)
+
     if isinstance(qones_range, int):
         qones_list = [int(qones_range)]
     elif qones_range is None:
@@ -90,19 +128,22 @@ def build_npn_classes(qargs, qones_range=None, **kwargs):
     else:
         qones_list = list(qones_range)
 
-    num = 0
+    values = { 'qargs': qargs, 'num': 0 }
     bits = BitsInNumpy(qfunctions)
     for qones in qones_list:
+        values['qones'] = qones
         func = 2 ** qones - 1
         func_last = 2 ** (qvalues-1)
         while func < func_last:
             if bits.append(func):
-                npn_class_len = 1 + get_npn_class(qvalues, func, transforms, bits)
-                num += 1
-                func_str = f"{func:b}".zfill(qvalues)
-                print(f"{num:6d} {func_str} {npn_class_len:4d}", flush=True, **kwargs)
+                values['npn_class_len'] = 1 + get_npn_class(qvalues, func, transforms, bits)
+                values['num'] += 1
+                values['func'] = func
+                print(*_out(out_format, **values), flush=True, **kwargs)
+
                 if func == 0:
                     break
+
             func = ncm(func)
 
 
@@ -136,6 +177,39 @@ def make_todo(arg): # pylint: disable=too-many-return-statements
     return int(qargs), sorted(set(qones_range))
 
 
+def parse_out_format(fmt):
+    """
+    Parse format string and return.
+    """
+    result = []
+    for ch in fmt:
+        if ch in FORMATS:
+            result.append([ch, ''])
+        elif result and ch.isdigit():
+            result[-1][1] += ch
+        else:
+            return f"Invalid char {ch} in format"
+
+    return [ (ch, int(width) if width else None) for ch, width in result ]
+
+
+def set_default_width(out_format, qvalues):
+    """
+    Set a default width for some formats
+    """
+    for i, (fmt, width) in enumerate(out_format):
+        if width is not None:
+            continue
+        if fmt == '#':
+            out_format[i] = (fmt, 6)
+        elif fmt == 'B':
+            out_format[i] = (fmt, qvalues)
+        elif fmt == 'H':
+            out_format[i] = (fmt, qvalues // 4 + bool(qvalues % 4))
+        elif fmt == 'S':
+            out_format[i] = (fmt, 4)
+
+
 def main():
     """
     An entry point.
@@ -152,6 +226,12 @@ def main():
             'nargs': 1,
             'help': 'Set output file',
         },
+        ('--format', '-f'): {
+            'nargs': 1,
+            'help': 'Output format FIELD [WIDTH] ... ' +
+                    'Fields are: # sequence numer, H func in hex, B func in binary, ' +
+                    'D func in decimal, N arg count, Q one bits count, S class size.'
+        },
     }
 
     parser = ArgumentParser(description='Generate NPN classes of boolean functions.')
@@ -161,16 +241,29 @@ def main():
         parser.add_argument(*args, **kwargs)
     args = parser.parse_args()
 
+    qerrors = 0
+
     todo = [ make_todo(arg) for arg in args.args ]
     errors = [ msg for msg in todo if isinstance(msg, str) ]
     if errors:
+        qerrors += len(errors)
         print('Some error(s) in argument counts has been found:', file=sys.stderr)
         for msg in errors:
             print(' ', msg, file=sys.stderr)
+
+    kwargs = dict()
+
+    if args.format:
+        out_format = parse_out_format(args.format[0])
+        if isinstance(out_format, str):
+            qerrors += 1
+            print('Error in format:' , out_format, file=sys.stderr)
+        kwargs['out_format'] = out_format
+
+    if qerrors > 0:
         parser.print_usage()
         sys.exit(1)
 
-    kwargs = dict()
     try:
         if args.output:
             fn = args.output[0]
